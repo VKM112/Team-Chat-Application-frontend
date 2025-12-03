81f3793e5d17a16bbc3ef9baef3b7fa1e68a29f2
Mohan <vkmohan112@gmail.com>
2025-12-03 10:48:09 +0530

    Update in socket.ts

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,7 +1,9 @@
 import { io, Socket } from 'socket.io-client';
 
 const SERVER_URL =
-  import.meta.env.VITE_API_SERVER ?? `http://localhost:${import.meta.env.VITE_API_PORT ?? '5002'}`;
+  import.meta.env.VITE_API_BASE_URL?.replace('/api', '') || 
+  import.meta.env.VITE_API_SERVER || 
+  `http://localhost:${import.meta.env.VITE_API_PORT ?? '5002'}`;
 
 export const createSocket = (token?: string): Socket => {
   return io(SERVER_URL, {
